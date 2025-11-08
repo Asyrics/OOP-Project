221770515b663cb99d4786b18839184522206c3b
Asyric <52230208+Asyrics@users.noreply.github.com>
2025-11-08 23:26:29 +0200

    Update TEST.java

--- POO_Project/src/main/java/com/mycompany/poo_project/TEST.java
+++ POO_Project/src/main/java/com/mycompany/poo_project/TEST.java
@@ -9,5 +9,11 @@ package com.mycompany.poo_project;
  * @author Pestily
  */
 public class TEST {
-    
+    private int nuuuget = 0;
+    private int nuu23get = 0;
+    private int nu12get = 0;
+    private int nu51uget = 0;
+    private int nu4uuget = 0;
+    private int nuuu6uuget = 0;
+    private int nuuu2get = 0;
 }
