6913df337b2be6453f744faed9c3ea2dba3dfc03
robertandrei2006 <gavrilescurobert2006@gmail.com>
2025-11-08 23:26:48 +0200

    test
    test

---
+++ POO_Project/src/main/java/com/mycompany/poo_project/pulonezi.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo_project;
+
+/**
+ *
+ * @author fl3dh
+ */
+public class pulonezi {
+    
+    public static void main(String[] arg){
+        
+        System.out.println("Pulonezi");
+    }
+    
+}
