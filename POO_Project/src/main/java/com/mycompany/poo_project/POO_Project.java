522b24f7f8599733a642d1d4bb9f008eba293f09
robertandrei2006 <gavrilescurobert2006@gmail.com>
2025-11-08 22:50:42 +0200

    Revert "1-4"
    This reverts commit 85951496bfbb5b2ea222ea7ed1b479a4e1e44f37.

--- POO_Project/src/main/java/com/mycompany/poo_project/POO_Project.java
+++ POO_Project/src/main/java/com/mycompany/poo_project/POO_Project.java
@@ -3,64 +3,14 @@
  */
 
 package com.mycompany.poo_project;
-import java.util.ArrayList;
 
 /**
  *
  * @author Pestily
  */
-    
 public class POO_Project {
-    
-    public static void vectorOchelari_VR(ArrayList<Gadgets> lista, String rezolutie, int fov)
-    {
-        for(Gadgets vr : lista)
-        {
-            if(vr instanceof Ochelari_VR)
-            {
-                Ochelari_VR vr1 = (Ochelari_VR) vr;
-                if(vr1.getRezolutie().equalsIgnoreCase(rezolutie) && vr1.getFov() == fov)
-                {
-                    System.out.println(vr1);
-                }
-            }
-        }
-    }
-    
-    public static void vectorBoxe_Portabile(ArrayList<Gadgets> lista, String rezistenta, int max_greutate)
-    {
-        for(Gadgets bp : lista)
-        {
-            if(bp instanceof Boxe_Portabile)
-            {
-                Boxe_Portabile bp1 = (Boxe_Portabile) bp;
-                if(bp1.getRezistenta().equalsIgnoreCase(rezistenta) && bp1.getGreutate() <= max_greutate)
-                {
-                    System.out.println(bp1);
-                }
-            }
-        }
-    }
 
     public static void main(String[] args) {
-        
-        ArrayList<Gadgets> listaBoxe_Portabile = new ArrayList<>();
-        ArrayList<Gadgets> listaOchelari_VR = new ArrayList<>();
-   
-        for(int i = 0 ; i <10 ; i++)
-        {
-            Ochelari_VR vr1 = new Ochelari_VR("Quest 3", "Alb", "Meta",true,"1920x1080p",90,120,"Titanium","Uifi");
-            listaOchelari_VR.add(vr1);
-        }
-    
-    
-        for(int i = 0 ; i <10 ; i++)
-        {
-            Boxe_Portabile bp1 = new Boxe_Portabile("Sony", "Negru","JBL",false,"la apa", 20,3,"310 mm latime","Acumulator");
-            listaBoxe_Portabile.add(bp1);
-        }
-        
-        vectorOchelari_VR(listaOchelari_VR,"1920x1080p",120);
+        System.out.println("Hello World!");
     }
-    
 }
