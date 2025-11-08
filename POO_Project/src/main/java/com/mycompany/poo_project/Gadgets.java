511ede7b17bd3228381151c3507f78e558136f94
Asyric <52230208+Asyrics@users.noreply.github.com>
2025-11-08 19:34:35 +0200

    Start
    Clasa abstracta Gadgets comuna

---
+++ POO_Project/src/main/java/com/mycompany/poo_project/Gadgets.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo_project;
+
+/**
+ *
+ * @author Pestily
+ */
+public abstract class Gadgets {
+    public int pret = 0;
+    
+    
+    public abstract int GetPret();
+}
