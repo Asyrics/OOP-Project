7b75f59b2b0ea89f1b17f4720c46b0e3dc24984e
Asyric <52230208+Asyrics@users.noreply.github.com>
2025-11-08 23:17:11 +0200

    merge

---
+++ POO_Project/src/main/java/com/mycompany/poo_project/Ochelari_VR.java
@@ -0,0 +1,140 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo_project;
+
+/**
+ *
+ * @author fl3dh
+ */
+public class Ochelari_VR extends Gadgets 
+{
+    // proprietati >:)
+    private String rezolutie = null;
+    private int rata_refresh = 0;
+    private int fov = 0;
+    private String material = null;
+    private String tip_conexiune = null;
+    
+    //constructor
+    
+    public Ochelari_VR(){
+        rezolutie = null;
+        rata_refresh = 0;
+        fov = 0;
+        material = null;
+        tip_conexiune = null;
+    }
+    
+    //constructor cu parametrii
+    public Ochelari_VR(String nume, String culoare, String brand, boolean software,String rezolutie, 
+            int rata_refresh, int fov, String material,String tip_conexiune)
+    {
+        super.numeProdus = nume;
+        super.culoare = culoare;
+        super.brand = brand;
+        super.software = software;
+        this.rezolutie = rezolutie;
+        this.rata_refresh = rata_refresh;
+        this.fov = fov;
+        this.material = material;
+        this.tip_conexiune = tip_conexiune;
+    }
+    
+    //constructor de copiere
+    public Ochelari_VR(Ochelari_VR a1)
+    {
+        super.numeProdus = a1.numeProdus;
+        super.culoare = a1.culoare;
+        super.brand = a1.brand;
+        super.software = a1.software;
+        this.rezolutie = a1.rezolutie;
+        this.rata_refresh = a1.rata_refresh;
+        this.fov = a1.fov;
+        this.material = a1.material;
+        this.tip_conexiune = a1.tip_conexiune;
+    }
+    
+    //setteri
+    public void setRezolutie(String rez)
+    {
+        this.rezolutie = rez;
+        
+    }
+    
+    public void setRataRefresh(int refresh)
+    {
+        this.rata_refresh = refresh;
+    }
+    
+    public void setFov(int fov1)
+    {
+        this.fov = fov1;
+    }
+    
+    public void setMaterial(String material1)
+    {
+        this.material = material1;
+    }
+    
+    public void setTipConexiune(String conexiune)
+    {
+        this.tip_conexiune = conexiune;
+    }
+    
+    //getteri
+    
+    public String getRezolutie()
+    {
+        return this.rezolutie;
+    }
+    
+    public int getRataRefresh()
+    {
+        return this.rata_refresh;
+    }
+    
+    public int getFov()
+    {
+        return this.fov;
+    }
+    
+    public String getMaterial()
+    {
+        return this.material;
+    }
+    
+    public String getTipConexiune()
+    {
+        return this.tip_conexiune;
+    }
+    
+    //Overrides
+    @Override
+    public int getPret() 
+    {
+     return 0;
+    }
+    
+    @Override 
+    public boolean esteSmart() 
+    {
+     return false;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return super.toString()+ " "+ rezolutie + " " + rata_refresh + " " + fov + " " + material + " "+ tip_conexiune;
+            
+    }
+    
+}
+    
+    
+    
+    
+    
+    
+
