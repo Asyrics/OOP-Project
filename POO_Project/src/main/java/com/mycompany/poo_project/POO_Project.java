d3b2f496a73af6fabadd8263a4e7a48d21f898d0
Asyric <52230208+Asyrics@users.noreply.github.com>
2025-11-08 23:13:57 +0200

    Revert "MERGE"
    This reverts commit 890c1806030b7cb5040ecc1937d9a79f5064bc24.

--- POO_Project/src/main/java/com/mycompany/poo_project/POO_Project.java
+++ POO_Project/src/main/java/com/mycompany/poo_project/POO_Project.java
@@ -12,29 +12,6 @@ import java.util.ArrayList;
     
 public class POO_Project {
     
-    public static void afisareVectorAlarme(ArrayList<Gadgets> lista, String tipd, String Brand) {
-            System.out.println("Alarme ce au detectia " + tipd + " si sunt de la brandul " + Brand);
-            for(Gadgets a : lista){
-                if(a instanceof Alarma){
-                    Alarma a1 = (Alarma) a;
-                    if(a1.getTipDet().equalsIgnoreCase(tipd) && a1.getBrand().equalsIgnoreCase(Brand)){
-                        System.out.println(a1);
-                    }
-                }
-            }
-    }
-    public static void afisareVectorPrize(ArrayList<Gadgets> lista, int nrprize, String Brand) {
-            System.out.println("Prize ce au numarul de prize " + nrprize + " si de la brandul: " + Brand);
-            for(Gadgets p : lista){
-                if(p instanceof PrizaSmart){
-                    PrizaSmart p1 = (PrizaSmart) p;
-                    if(p1.getNrPrize() == nrprize && p1.getBrand().equalsIgnoreCase(Brand)){
-                        System.out.println(p1);
-                    }
-                }
-            }
-    }
-    
     public static void vectorOchelari_VR(ArrayList<Gadgets> lista, String rezolutie, int fov)
     {
         for(Gadgets vr : lista)
@@ -67,9 +44,6 @@ public class POO_Project {
 
     public static void main(String[] args) {
         
-        //vectori
-        ArrayList<Gadgets> listaAlarme = new ArrayList<>();
-        ArrayList<Gadgets> listaPrize = new ArrayList<>();
         ArrayList<Gadgets> listaBoxe_Portabile = new ArrayList<>();
         ArrayList<Gadgets> listaOchelari_VR = new ArrayList<>();
    
@@ -87,22 +61,6 @@ public class POO_Project {
         }
         
         vectorOchelari_VR(listaOchelari_VR,"1920x1080p",120);
-        
-        //Alarma
-        // String nume,String cul,boolean soft,String Brand,String tipd,String tipa,String tipm,int greutate,String dim
-        //Priza
-        //String nume,String cul,boolean soft,String Brand,String tipd,String tipa,String tipm,int greutate,String dim
-        for(int i = 0 ; i<10 ;i++){
-            Alarma a = new Alarma("ART-ARC3800H-03-W2","Alb",false,"Dahua","PIR","Wireless","Perete",510,"74.8×174.8×38.3");
-            listaAlarme.add(a);
-        }
-        for(int i = 0 ; i<10 ;i++){
-            PrizaSmart p = new PrizaSmart("G20","Alb",true,"GAVIO",1,20,240,"Suprasarcina","European");
-            listaPrize.add(p);
-        }
-        
-        afisareVectorAlarme(listaAlarme,"PIR","Dahua");
-        afisareVectorPrize(listaPrize,1,"GAVIO");
     }
     
 }
